83b5fc8b9112358b3dcbce6380d17b957b10fe9d
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-09-25 21:24:53 +0300

    Исправил некоторые ошибки вывода масива

--- src/by/home/maxzzzit/VivodMassivaNaFormu.java
+++ src/by/home/maxzzzit/VivodMassivaNaFormu.java
@@ -1,49 +1,50 @@
 package by.home.maxzzzit;
 
-import java.util.Arrays;
 import java.util.Scanner;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VivodMassivaNaFormu extends JFrame{
-	public static JLabel vivod2 = new JLabel();
 	public static void main(String[]args) {
 		Scanner scn = new Scanner(System.in);
-		int razm;
+		int razmer;
 		System.out.print("Введите размер массива: ");
-		razm = scn.nextInt();
-		int [] massiv = new int [razm];
+		razmer = scn.nextInt();
+		int [] massiv = new int [razmer];
+
 		VivodMassivaNaFormu bumaga = new VivodMassivaNaFormu();
-		JPanel pan = new JPanel();
-		JLabel vivod1 = new JLabel();
+		JPanel panel = new JPanel();
+		JLabel vivod = new JLabel();
+		JLabel[] labels = new JLabel[razmer];
 
 		bumaga.setTitle("Массив");
-		bumaga.setSize(300, 400);
-		bumaga.add(pan);
-		bumaga.setVisible(true);
-		pan.add(vivod1);
-		pan.add(vivod2);
-		vivod1.setText("Результат : ");
-//		vivod1.setVisible(true);
+		bumaga.setSize(130, 400);
+		bumaga.setDefaultCloseOperation(VivodMassivaNaFormu.EXIT_ON_CLOSE);
 		bumaga.setLocationRelativeTo(null);
-		zapolnenie(massiv);
-		rezultat(massiv);
-//		String otvet = String.valueOf(rezultat(massiv));
-//		vivod2.setText(otvet);
+		vivod.setText("Результат : ");
+
+		bumaga.add(panel);
+		panel.add(vivod);
+
+		zapolnenie(massiv,labels,panel);
+		rezultat(massiv,labels);
+		bumaga.setVisible(true);
 	}
-	
-	private static void zapolnenie (int [] mass) {
+
+	private static void zapolnenie (int [] mass, JLabel [] lab, JPanel panel) {
 		for (int i = 0; i < mass.length; i++) {
 			mass[i] = (int)(10+Math.random()*89);
+
+			lab[i] = new JLabel();
+			lab[i].setText((i+1)+") - " + mass[i]+", ");
+			panel.add(lab[i]);
 		}
 	}
-	
-	private static void rezultat (int [] mass) {
+
+	private static void rezultat (int [] mass, JLabel [] lab) {
 		for (int i = 0; i < mass.length; i++) {
-			vivod2.setText(mass[i]+" ");
-			System.out.print(mass[i]+" ");
+			System.out.println(lab[i].getText());
 		}
 	}
 }
